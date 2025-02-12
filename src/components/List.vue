f28fd1b3d8d70d997882daf849b3e9a54e0e6246
davidhalasz <davidhalasz22@gmail.com>
2025-02-12 10:08:52 +0100

    forgot to add checked

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,7 +3,7 @@
         <h1 class="py-4 text-2xl font-bold">TODO list</h1>
         <ul>
             <li class="flex  items-center gap-4 pt-2" v-for="(todo, index) in todoStore.todos" :key="index">
-                <input class="flex-none" type="checkbox" @change="toggleTodo(index)" />
+                <input class="flex-none" type="checkbox" :checked="todo.completed" @change="toggleTodo(index)" />
                 <p class="grow">{{ todo.text }}</p>
                 <button class="flex-none px-3 py-1 bg-red-500 rounded text-white" @click="removeTodo(index)">remove</button>
             </li>
